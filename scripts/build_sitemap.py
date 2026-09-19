b17a85828c92d517ca07d80406ef9008c557623e
alexyakovlev79 <alex.yakovlev79@gmail.com>
2026-09-19 16:10:35 +0300

    Repair sitemap lastmod generator

--- scripts/build_sitemap.py
+++ scripts/build_sitemap.py
@@ -12,7 +12,8 @@ JSONLD_RE = re.compile(
     r'<script[^>]+type=["\']application/ld\+json["\'][^>]*>([\s\S]*?)</script>',
     re.I,
 )
-DATE_RE = re.compile(r"\d{4}-\d{2}-\d{2}")\nSERVICE_PAGES = {"index.html", "ratings.html", "methodology.html", "404.html"}
+DATE_RE = re.compile(r"\d{4}-\d{2}-\d{2}")
+SERVICE_PAGES = {"index.html", "ratings.html", "methodology.html", "404.html"}
 
 
 def _types(node):
